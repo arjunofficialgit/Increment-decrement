bd0a93cd85e669676093c72145aa90f82ec9dc80
Arjun Singha <arjunofficial.tech@gmail.com>
2023-11-02 19:51:24 +0530

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+const countValue= document.querySelector('#counter');
+
+const increment = () => {
+    // get the value from ui
+    let value = parseInt(countValue.innerText);
+    // update value
+    value=value+1
+    // set the value onto ui
+    countValue.innerText = value;
+}
+
+const decrement = () => {
+    // get the value from ui
+    let value = parseInt(countValue.innerText);
+    // update value
+    value=value-1
+    // set the value onto ui
+    countValue.innerText = value;
+
+}
